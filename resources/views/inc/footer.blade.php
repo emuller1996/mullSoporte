f87b72d0d0bb77c5eaccf0ae633fd7848cc5e189
emuller1996 <stefano.muller@hotmail.com>
2022-04-29 23:48:51 -0500

    first commit

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,26 @@
+
+<footer class="p-4 bg-indigo-300 mt-6  shadow md:px-6 md:py-8 dark:bg-gray-800">
+    <div class="sm:flex sm:items-center sm:justify-between">
+    <a href="https://flowbite.com" class="flex items-center mb-4 sm:mb-0">
+    <img src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"" class="mr-3 h-8" alt="Flowbite Logo">
+    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">American Shop VIP.</span>
+    </a>
+    <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+    <li>
+    <a href="#" class="mr-4 hover:underline md:mr-6 ">Acerca.</a>
+    </li>
+    <li>
+    <a href="#" class="mr-4 hover:underline md:mr-6">Politicas de Privacidad</a>
+    </li>
+    <li>
+    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licencia</a>
+    </li>
+    <li>
+    <a href="#" class="hover:underline">Contacto</a>
+    </li>
+    </ul>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">MullerSoft.</a>. Todos los Derechos Reservados.
+    </span>
+    </footer>
---
+++ resources/views/team.blade.php
@@ -0,0 +1,21 @@
+@extends('inc.templade')
+
+
+@section('title', 'Equipo')
+
+@section('sidebar')
+    @parent
+    @include('inc.header')
+
+@endsection
+
+@section('content')
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+
+    </div>
+@endsection
+@section('footer')
+    @include('inc.footer')
+
+@endsection
